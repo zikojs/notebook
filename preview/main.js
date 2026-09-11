6a65d9ff3e3075f07cfa7ac3a16015bc96244dfc
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-09-11 09:18:15 +0100

    .

--- preview/main.js
+++
@@ -1,7 +0,0 @@
-import { FileBasedRouting } from "ziko";
-
-
-
-FileBasedRouting(import.meta.glob("./src/pages/**/*{.js,.mdz}"))
-// const pages = import.meta.glob("./src/pages/**/*.js")
-// console.log(Object.keys(pages))
